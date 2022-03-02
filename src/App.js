ea74ccb79f8af96c05f6c6e215155720c1ea1148
blm <mcleanb@photo-nomad.net>
2022-03-02 13:05:23 -0500

    testing

--- src/App.js
+++ src/App.js
@@ -4,20 +4,23 @@ import WordMap from "wordmap";
 import {getPredictions, initAlignmentMemory, initCorpus, initWordMap} from "./wordMapOps";
 import Permutations from "./components/Permutations";
 const alignment_data = require("./resources/alignments_for_eph.json");
-const map = initWordMap();
+const sourceVerseText = 'Παῦλος, ἀπόστολος ( οὐκ ἀπ’ ἀνθρώπων, οὐδὲ δι’ ἀνθρώπου, ἀλλὰ διὰ Ἰησοῦ Χριστοῦ, καὶ Θεοῦ Πατρὸς τοῦ ἐγείραντος αὐτὸν ἐκ νεκρῶν)';
+const targetVerseText = 'Paul, an apostle—not from men nor by man, but through Jesus Christ and God the Father, the one who raised him from the dead';
 
 export default function App() {
   const [results, setResults] = useState('');
+  const [wordmap, setWordMap] = useState(null);
 
   useEffect(async () => {
+    const map = initWordMap();
+
     // load WordMap with alignment data
     initAlignmentMemory(map, alignment_data);
     await initCorpus(map);
     
-    const sourceVerseText = 'Παῦλος, ἀπόστολος ( οὐκ ἀπ’ ἀνθρώπων, οὐδὲ δι’ ἀνθρώπου, ἀλλὰ διὰ Ἰησοῦ Χριστοῦ, καὶ Θεοῦ Πατρὸς τοῦ ἐγείραντος αὐτὸν ἐκ νεκρῶν)';
-    const targetVerseText = 'Paul, an apostle—not from men nor by man, but through Jesus Christ and God the Father, the one who raised him from the dead';
     const prediction = await getPredictions(map, sourceVerseText, targetVerseText);
     setResults(JSON.stringify(prediction));
+    setWordMap(map);
   }, [  ]);
 
 return (
@@ -26,11 +29,11 @@ return (
     <h2>Results:</h2>
     <div>{results}</div>
     <h2>Permutations:</h2>
-    {/*<Permutations*/}
-    {/*  wordmap={wordmap}*/}
-    {/*  sourceSentence={sourceVerseText}*/}
-    {/*  targetSenetence={targetVerseText}*/}
-    {/*/>*/}
+    <Permutations
+      wordmap={wordmap}
+      sourceSentence={sourceVerseText}
+      targetSenetence={targetVerseText}
+    />
   </div>
 );
 }
