2d3a9fc61cbf37d4dd623b9e225ffb7390e1dda8
Joshua Lansford <HootOwl7777@gmail.com>
2022-03-03 15:59:08 -0500

    Added learningRate to .05

--- src/brain_js_wordmap_test.js
+++ src/brain_js_wordmap_test.js
@@ -1,15 +1,17 @@
-brain = require( "brain.js" )
+const brain = require( "brain.js" )
+const fs = require('fs');
 
-use_gpu = false;
+use_gpu = true;
 
 // provide optional config object (or undefined). Defaults shown.
 // https://github.com/BrainJS/brain.js#training-options
 const config = {
     log: true,
     binaryThresh: 0.5, // ¯\_(ツ)_/¯
-    hiddenLayers: [10,20,10,5], // array of ints for the sizes of the hidden layers in the network
+    hiddenLayers: [26,30,10,5], // array of ints for the sizes of the hidden layers in the network
     activation: 'relu', // supported activation types: ['sigmoid', 'relu', 'leaky-relu', 'tanh']
-    iterations: 2000
+    iterations: 2000,
+    learningRate: 0.05
   };
   
   // create a simple feed forward neural network with backpropagation
@@ -23,8 +25,10 @@ const config = {
   const training_data = require( "../training_from_eph.json" );
   
   net.train(training_data);
+
+  input_data = training_data.map( x => x["input"] );
   
-  const output = net.run(training_data["input"]); // [0.987]
+  const output = net.run(input_data); // [0.987]
   
   console.log(output);
 
@@ -32,3 +36,4 @@ const config = {
   //https://github.com/BrainJS/brain.js#json
   //  const json = net.toJSON();
   //   net.fromJSON(json);
+  fs.writeFileSync( "brainjs_trained.json", JSON.stringify(net.toJSON()) );
