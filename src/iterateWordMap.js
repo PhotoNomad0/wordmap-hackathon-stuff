33b05c5f00e9af7d04428cbad010ea11420ce33d
blm <mcleanb@photo-nomad.net>
2022-03-09 07:04:04 -0500

    add suggestion counts

--- src/iterateWordMap.js
+++ src/iterateWordMap.js
@@ -159,9 +159,9 @@ export function loadTargetAndSource(baseFolder, bookId, chapterCount) {
 }
 
 export async function doWordMapIterations(parameter = 'alignmentPosition', start = 0.1, end = 1, stepSize = 0.1) {
-  const chapterCount = 6;
-  const doAlignments = true;
-  const bookId = 'eph';
+  const chapterCount = 24;
+  const doAlignments = false;
+  const bookId = 'luk';
   const recording = [];
   const wordMapOpts = {
     targetNgramLength: 5,
